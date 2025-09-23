0fcfe9a857bdf37f8ac55133d9764e64e1de50a6
Deepak1999 <73517127+Deepak1999@users.noreply.github.com>
2025-09-23 17:21:01 +0530

    commit 4th on 23rd sep 2025

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -11,27 +11,31 @@ const Login = () => {
     };
 
     return (
-        <div className="login-container">
-            <div className="back-button">
-                <i className="bi bi-chevron-left" onClick={handleContinue}></i>
-            </div>
-            <h1 className="login-title">Log in</h1>
-            <h3 className="login-subtitle">Sign in using your email to continue us</h3>
-            <form>
-                <div className="mb-4">
-                    <label for="email" className="form-label">Your email</label>
-                    <input type="email" className="form-control" id="email" required />
+        <div className='container' style={{ height: "100vh" }}>
+            <div className="login-container login">
+                <div className="back-button">
+                    <i className="bi bi-chevron-left" onClick={handleContinue}></i>
                 </div>
-                <div className="mb-3">
-                    <label for="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="password" required />
-                </div>
-                <a href="" className="forgot-password-link">Forgot Password?</a>
+                <div className='sign_content login'>
+                    <h1 className="login-title login">Log in</h1>
+                    <h3 className="login-subtitle login">Sign in using your email to continue us</h3>
+                    <form>
+                        <div className="mb-4">
+                            <label for="email" className="form-label">Your email</label>
+                            <input type="email" className="form-control" id="email" required />
+                        </div>
+                        <div className="mb-3">
+                            <label for="password" className="form-label">Password</label>
+                            <input type="password" className="form-control" id="password" required />
+                        </div>
+                        <a href="" className="forgot-password-link login">Forgot Password?</a>
 
-                <div className="d-grid">
-                    <button type="submit" className="btn btn-login">Log in</button>
+                        <div className="d-grid email_signup">
+                            <button type="submit" className="btn btn-login login">Log in</button>
+                        </div>
+                    </form>
                 </div>
-            </form>
+            </div>
         </div>
     );
 };
